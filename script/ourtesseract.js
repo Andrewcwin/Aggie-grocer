fdbaa4506f20b760c05db662a56ad3930c3755b5
eric-love02 <eric.love02@tamu.edu>
2022-10-01 23:44:18 -0500

    added shii

--- script/ourtesseract.js
+++ script/ourtesseract.js
@@ -10,5 +10,21 @@ fileSelector.addEventListener('change', (event) => {
         { logger: m => document.getElementById('display-image').innerHTML = m.status }
     ).then(({ data: { text } }) => {
         document.getElementById('display-image').innerHTML = text;
+
+        // get usable text
+
+        var arr = ['foo', 'bar', 'baz']; // ingredients as string[]
+        var json_str = JSON.stringify(arr);
+        setCookie('ingredients', json_str);
     })
 });
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
