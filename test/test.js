58f23b0237a9b769e9979e570217fd4f3fe815ee
yuki <73154239+YukiAtTamu@users.noreply.github.com>
2022-10-02 00:36:58 -0500

    added test search bar

--- test/test.js
+++ test/test.js
@@ -1,22 +1,15 @@
-// View complete code at: https://github.com/Asprise/receipt-ocr/tree/main/javascript-nodejs-receipt-ocr
-console.log("=== JavaScript/Node.js Receipt OCR ===");
-
-var receiptOcrEndpoint = 'https://ocr.asprise.com/api/v1/receipt';
-var imageFile = 'US-1.jpg'; // Modify it to use your own file
-
-var fs = require('fs');
-var request = require('request');
-request.post({
-  url: receiptOcrEndpoint,
-  formData: {
-    api_key: 'TEST',        // Use 'TEST' for testing purpose
-    recognizer: 'auto',        // can be 'US', 'CA', 'JP', 'SG' or 'auto'
-    ref_no: 'ocr_nodejs_123', // optional caller provided ref code
-    file: fs.createReadStream(imageFile) // the image file
-  },
-}, function(error, response, body) {
-  if(error) {
-    console.error(error);
+// JavaScript code
+function search_recipe() {
+  let input = document.getElementById('searchbar').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('recipes');
+    
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="list-item";                 
+      }
   }
-  console.log(body); // Receipt OCR result in JSON
-});
+}
